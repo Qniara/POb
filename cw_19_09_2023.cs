37c50c85923c28261b57b1c3b5b4b6beeb9e66fc
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-09-19 12:00:31 +0200

    Update cw_19_09_2023.cs

--- cw_19_09_2023.cs
+++ cw_19_09_2023.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace cw_19_09_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kathrine Pearce <3");
+            Console.WriteLine("Damonnnn Salvatoree");
+            Console.WriteLine("Klaus Mikealson");
+            //Console.Clear(); -> czyści ekran
+            //Console.ReadKey(); -> podanie jaki klawisz ma być kliknięty aby
+            //zakończyć wyświetlanie konsoli
+        }
+    }
+}
