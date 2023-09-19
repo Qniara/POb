62aedd13e13f3e59d1f22571471d28a8df1eb583
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-09-19 12:17:59 +0200

    Update cw_19_09_2023.cs

--- cw_19_09_2023.cs
+++ cw_19_09_2023.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,19 +12,28 @@ namespace cw_19_09_2023
         static void Main(string[] args)
         {
             Console.WriteLine("Kathrine Pearce <3");
-            Console.WriteLine("Damonnnn Salvatoree");
-            Console.WriteLine("Klaus Mikealson");
             //Console.Clear(); -> czyści ekran
             //Console.ReadKey(); -> podanie jaki klawisz ma być kliknięty aby
             //zakończyć wyświetlanie konsoli
 
             //TYPY DANYCH
             /*
-             * INT
+             * int 16, 32, 64
+             * byte -> zmienna liczbowa o wartości od 0 do 255
+             * sbyte - > zmienna liczbowa o wartości od -128 do 127
+             * short -> zmienna liczbowa o wartości od -32768 do 32767
+             * int -> zmienna liczbowa o wartości od -2147483648 do 2147483647
              */
 
-            int i; //deklaracja zmiennej
+            int i = 27; //deklaracja zmiennej
             int j = 10; //deklaracja z inicjalozacją
+            System.Int32 x = 10;
+            Console.WriteLine("Zmienna j wynosi {0}, zmienna x wynosi {1}", j, x);
+            Console.WriteLine($"Zmienna i wynosi {i}, \nzmienna j wynosi {j}");
+
+            byte b = 27;
+            sbyte sb = -10;
+            short s = -27;
         }
     }
 }
