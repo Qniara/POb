a711ec910ff5850446d7751af282aea0224f9ac7
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-10-17 11:05:50 +0200

    Update cw_17_10_2023.cs

--- cw_17_10_2023.cs
+++ cw_17_10_2023.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+
 namespace cw_17._10._2023
 {
     internal class Program
@@ -15,13 +17,49 @@ namespace cw_17._10._2023
             //    Console.WriteLine("Błędne dane");
             //}
 
-            Console.WriteLine("Podaj a: ");
+            Console.Write("Podaj a: ");
             double a;
-            while (!double.TryParse(Console.ReadLine(), out a))
+            while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
             {
                 Console.WriteLine("Wpisz prawidłowe dane.");
             }
-
+            Console.Write("Podaj b: ");
+            double b;
+            while (!double.TryParse(Console.ReadLine(), out b) || b <= 0)
+            {
+                Console.WriteLine("Wpisz prawidłowe dane.");
+            }
+            Console.Write("Podaj c: ");
+            double c;
+            while (!double.TryParse(Console.ReadLine(), out c) || c <= 0)
+            {
+                Console.WriteLine("Wpisz prawidłowe dane.");
+            }
+            if (IsTriangle(a, b, c))
+            {
+                Console.WriteLine($"Pole trojkata o bokach {a}, {b} i {c} wynosi: {Pole(a,b,c)}");
+            }
+            else
+            {
+                Console.WriteLine("Podane dlugosci nie tworza trojboka.");
+            }
+            bool IsTriangle(double a, double b, double c)
+            {
+                if(a+b>c && b+c>a && c + a > b)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            double Pole(double a, double b, double c)
+            {
+                double p = (a + b + c) / 2;
+                double pole = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return pole;
+            }
         }
     }
 }
