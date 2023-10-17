b107ff91726b766415bab401a877c6f908757fb9
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-10-17 11:23:13 +0200

    Update cw_17_10_2023.cs

--- cw_17_10_2023.cs
+++ cw_17_10_2023.cs
@@ -16,33 +16,36 @@ namespace cw_17._10._2023
             //{
             //    Console.WriteLine("Błędne dane");
             //}
-
-            Console.Write("Podaj a: ");
-            double a;
-            while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+            bool isCorrect = false;
+            do
             {
-                Console.WriteLine("Wpisz prawidłowe dane.");
-            }
-            Console.Write("Podaj b: ");
-            double b;
-            while (!double.TryParse(Console.ReadLine(), out b) || b <= 0)
-            {
-                Console.WriteLine("Wpisz prawidłowe dane.");
-            }
-            Console.Write("Podaj c: ");
-            double c;
-            while (!double.TryParse(Console.ReadLine(), out c) || c <= 0)
-            {
-                Console.WriteLine("Wpisz prawidłowe dane.");
-            }
-            if (IsTriangle(a, b, c))
-            {
-                Console.WriteLine($"Pole trojkata o bokach {a}, {b} i {c} wynosi: {Pole(a,b,c)}");
-            }
-            else
-            {
-                Console.WriteLine("Podane dlugosci nie tworza trojboka.");
-            }
+                Console.Write("Podaj a: ");
+                double a;
+                while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+                {
+                    Console.WriteLine("Wpisz prawidłowe dane.");
+                }
+                Console.Write("Podaj b: ");
+                double b;
+                while (!double.TryParse(Console.ReadLine(), out b) || b <= 0)
+                {
+                    Console.WriteLine("Wpisz prawidłowe dane.");
+                }
+                Console.Write("Podaj c: ");
+                double c;
+                while (!double.TryParse(Console.ReadLine(), out c) || c <= 0)
+                {
+                    Console.WriteLine("Wpisz prawidłowe dane.");
+                }
+                if (IsTriangle(a, b, c))
+                {
+                    Console.WriteLine($"Pole trojkata o bokach {a}, {b} i {c} wynosi: {Pole(a,b,c)}");
+                }
+                else
+                {
+                    Console.WriteLine("Podane dlugosci nie tworza trojboka.");
+                }
+            }while(!isCorrect);
             bool IsTriangle(double a, double b, double c)
             {
                 if(a+b>c && b+c>a && c + a > b)
