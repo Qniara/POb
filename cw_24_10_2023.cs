c9a4c97936b593bf213f7464fafc64e20e26ff68
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-10-24 10:15:10 +0200

    Update cw_24_10_2023.cs

--- cw_24_10_2023.cs
+++ cw_24_10_2023.cs
@@ -1,4 +1,4 @@
-using System;
+using System; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,19 @@ namespace defekacja
     {
         static void Main(string[] args)
         {
+            //TRY CATCH
+            try
+            {
+                Console.WriteLine("Podaj dwie liczby\n");
+                Console.WriteLine("Podaj a: ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj y:");
+                double y = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{x}/{y}={x / y:F3}");
+            }catch(Exception ex)
+            {
+                Console.WriteLine($"Blad: {ex.Message}");
+            }
         }
     }
 }
