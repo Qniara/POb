2ef4ba9912ae8ac1add9e6c804b7a7636af80c6d
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-11-14 09:50:24 +0100

    Create cw_14_11_2023.cs

---
+++ cw_14_11_2023.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_14_11_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
