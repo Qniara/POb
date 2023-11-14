df71841dfc64f48d45f257e0019d718897435ce7
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-11-14 10:45:56 +0100

    Update cw_14_11_2023.cs

--- cw_14_11_2023.cs
+++ cw_14_11_2023.cs
@@ -8,8 +8,38 @@ namespace cw_14_11_2023
 {
     internal class Program
     {
+        //funkcja statyczna, która pozwala wprowadzić rozmiar tablicy i nazwę - CreateArray()
+        static int CreateArray()
+        {
+            Console.WriteLine("Podaj rozmiar: ");
+            int size = int.Parse(Console.ReadLine());
+            return size;
+        }
+        //funkcja statyczna, która ustawia elementy tablicy SetArray()
+        static void SetArray(int[] T)
+        {
+            int size = T.Length;
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Podaj liczbe: ");
+                int x = int.Parse(Console.ReadLine());
+                T[i] = x;
+            }
+        }
+        //funkcja statyczna, która wyświetla zawartość tablicy DisplayArray()
+        static void DisplayArray(int[] T)
+        {
+            for(int i=0;i < T.Length;i++)
+            {
+                Console.Write(T[i] + " ");
+            }
+        }
         static void Main(string[] args)
         {
+
+            int[] T= new int[CreateArray()];
+            SetArray(T);
+            DisplayArray(T);
         }
     }
 }
