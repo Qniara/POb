2a146bf9f54efcb2d56ab2a8afe227d8e1f2d9b0
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-11-25 12:54:55 +0100

    Update pupa1.cs

--- zadania_cw/pupa1.cs
+++ zadania_cw/pupa1.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -38,14 +40,31 @@ namespace Pob_cw_pupciasne
         }
         static void Main(string[] args)
         {
-            int[] T= CreateArray();
-            SetArray(T);
-            DisplayArray(T);
+            Console.OutputEncoding = Encoding.UTF8;
+            //int[] T= CreateArray();
+            //SetArray(T);
+            //DisplayArray(T);
+
             //Napisz program, który pobiera od użytkownika 5 liczb całkowitych i zapisuje je w tablicy.Następnie program
             //wyświetla sumę wszystkich elementów tablicy. W celu zabezpieczenia programu przed niepoprawnymi danymi
             //wejściowymi, użyliśmy metody int.TryParse(), która pozwala na sprawdzenie, czy wprowadzona wartość jest
             //liczbą całkowitą.Jeśli użytkownik wprowadzi niepoprawną wartość, program wyświetli stosowny komunikat i
             //poprosi o ponowne wprowadzenie liczby.
+            //int suma = 0;
+            //int n = 0;
+            //int[] T = new int[5];
+            //int i = 0;
+            //while(i!=5)
+            //{
+            //    Console.WriteLine($"Podaj {i} element tablicy: ");
+            //    while (!int.TryParse(Console.ReadLine(), out n)){
+            //        Console.WriteLine("Nie pyskuj bębnie tylko daj liczbe: ");
+            //    }
+            //    T[i] = n;
+            //    suma = suma + n;
+            //    i++;
+            //}
+            //Console.WriteLine($"Suma elementow tablicy to: " + suma);
 
             //Napisz program, który pobiera od użytkownika 5 liczb całkowitych i zapisuje je w tablicy, a następnie wyświetla
             //sumę wszystkich elementów tablicy.Zadeklaruj tablicę o rozmiarze 5 i nazwij ją numbers. Użyj pętli for, aby
@@ -58,7 +77,48 @@ namespace Pob_cw_pupciasne
             //tablicę liczb całkowitych i zwraca ich sumę. Użyj pętli foreach, aby dodać do sumy każdy element tablicy numbers
             //i zwrócić sumę. Wywołaj metodę CalculateSum(numbers) i przypisz jej wynik do zmiennej sum. Wyświetl na ekranie
             //komunikat, który pokazuje sumę podanych liczb.
-            //int GetIntegerFromUser()
+            int suma = 0;
+            int n = 0;
+            int[] T = new int[5];
+            int[] numbers = new int[5];
+            int i = 0;
+            while (i != 5)
+            {
+                Console.WriteLine($"Podaj {i} element tablicy: ");
+                while (!int.TryParse(Console.ReadLine(), out n))
+                {
+                    Console.WriteLine("Nie pyskuj bębnie tylko daj liczbe: ");
+                }
+                T[i] = n;
+                suma = suma + n;
+                i++;
+            }
+            Console.WriteLine($"Suma elementow tablicy to: " + suma);
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine($"Podaj {j} element tablicy: ");
+                numbers[j] = GetIntegerFromUser();
+            }
+            int sum = CalculateSum(numbers);
+            Console.WriteLine($"Suma elementow tablicy numbers to: {sum}.");
+        }
+        public static int GetIntegerFromUser()
+        {
+            int n = 0;
+            while(!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Nie cwaniakuj tylko liczbe dawaj ");
+            }
+            return n;
+        }
+        public static int CalculateSum(int[] numbers)
+        {
+            int suma = 0;
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                suma = suma + numbers[i];
+            }
+            return suma;
         }
     }
 }
