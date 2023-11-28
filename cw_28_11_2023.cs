8f8c6ee3578cf9181ca08e2bc10a6e13c1aa248d
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-11-28 10:17:43 +0100

    Update cw_28_11_2023.cs

--- cw_28_11_2023.cs
+++ cw_28_11_2023.cs
@@ -1,1 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tablice dwuwymiarowe
+            //int[,] ints = new int[4, 3];
+            //Console.WriteLine(ints.Rank);//podaje ilu wymiarowa tablica
+            //Console.WriteLine(ints.GetLength(0));//podaje dlugosc tablicy na pierwszym wymiarze
+            //Console.WriteLine(ints.GetLength(1));//podaje dlugosc tablicy na drugim wymiarze
+
+            //for(int i=0; i<ints.GetLength(0); i++)
+            //{
+            //    for(int j=0; j < ints.GetLength(1); j++)
+            //    {
+            //        Console.Write($"ints[{i},{j}]={ints[i,j]}  ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            int[,,] cubs = new int[4, 3, 2];
+            for (int i = 0; i < cubs.GetLength(0); i++)
+            {
+                for (int j = 0; j < cubs.GetLength(1); j++)
+                {
+                    for(int k = 0; k<cubs.GetLength(2); k++)
+                    {
+                        Console.Write($"cubs[{i},{j},{k}]={cubs[i, j, k]}  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
