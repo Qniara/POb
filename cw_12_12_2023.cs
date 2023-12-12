86e3f222d31a2c3fd8a0c784bb37563d78abae45
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-12-12 14:15:52 +0300

    Update cw_12_12_2023.cs

--- cw_12_12_2023.cs
+++ cw_12_12_2023.cs
@@ -80,6 +80,58 @@ namespace _12._12._23r
             //{
             //    Console.WriteLine($"Kolor: {color.Key}\nW RGB: {color.Value}\n");
             //}
+
+            //Popros użytkownika o podanie par klusz i wartosc
+            //Dictionary<string, string> data = new Dictionary<string, string>();
+            //Console.WriteLine("Podaj ile chcesz wprowadzić: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for(int i=0; i < n;i++)
+            //{
+            //    Console.Write("Podaj klucz: ");
+            //    string klucz = Console.ReadLine();
+            //    Console.Write("Podaj wartosc: ");
+            //    string wartosc = Console.ReadLine();
+            //    data.Add(klucz, wartosc);
+            //}
+            //foreach(var item in data)
+            //{
+            //    Console.WriteLine($"Klucz: {item.Key}\nWartosc: {item.Value}\n");
+            //}
+
+            //Napisz funkcję, która przyjmuje jako argument listę liczb całkowitych i zwraca słownik, w
+            //którym kluczem jest liczba, a wartością jest jej częstotliwość występowania na liście.
+            //Jeśli lista jest pusta lub null, funkcja powinna zwrócić pusty słownik.
+            //Przykład: dla listy[1, 2, 3, 2, 4, 1, 5, 2] funkcja powinna zwrócić słownik
+            //{ 1: 2, 2: 3, 3: 1, 4: 1, 5: 1}.Napisz program, który wczytuje od użytkownika ciąg znaków,
+            //próbuje przekonwertować go na liczbę całkowitą za pomocą metody TryParse, a następnie wyświetla
+            //wynik na konsoli. Jeśli konwersja się powiedzie, program powinien wyświetlić liczbę i
+            //informację, że jest to poprawna liczba całkowita. Jeśli konwersja się nie powiedzie, program
+            //powinien wyświetlić informację, że podany ciąg znaków nie jest poprawną liczbą całkowitą.
+            //Przykład: dla ciągu znaków “123” program powinien wyświetlić “123 jest poprawną liczbą
+            //całkowitą”.Dla ciągu znaków “abc” program powinien wyświetlić “abc nie jest poprawną liczbą
+            //całkowitą”.
+        }
+        Dictionary<int, int> funkcja(List<int> Lista)
+        {
+            Dictionary<int, int> slownik = new Dictionary<int, int>();
+            List<int> Lista2 = new List<int>();
+            int x = 0;
+            for(int i = 0; i < Lista.Count; i++)
+            {
+                for(int j = 0; j < Lista.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        x++;
+                    }
+                }
+                Lista2.Add(x);
+            }
+            for(int i=0; i < Lista2.Count; i++)
+            {
+                slownik.Add(Lista[i], Lista2[i]);
+            }
+            return slownik;
         }
     }
 }
