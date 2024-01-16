8885bc9db0174b4db005331e3d00590358a1a30f
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-16 12:12:59 +0100

    Update cw_16_01_2024.cs

--- Lekcje/cw_16_01_2024.cs
+++ Lekcje/cw_16_01_2024.cs
@@ -98,3 +98,31 @@ namespace Szyper_16_01_2024
         }
     }
 }
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szyper_16_01_2024_2
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public float Height { get; set; }
+            public float Weight { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Person KurtCobain = new Person();
+            //Obiekt klasy person posiada walsciwoszi i metody (dopytac starego)
+        }
+    }
+}
