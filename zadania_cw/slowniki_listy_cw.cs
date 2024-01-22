a271d76ca30d6c8a83ef377462a8d73bbcbacca5
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-22 19:23:15 +0100

    Update slowniki_listy_cw.cs

--- zadania_cw/slowniki_listy_cw.cs
+++ zadania_cw/slowniki_listy_cw.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace szyper_sloniki_cw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<int, int>boki= new Dictionary<int, int>();
+            //List<int> pola=new List<int>();
+            //Random r= new Random();
+            //for(int i=0;i<5;i++)
+            //{
+            //    boki.Add(r.Next(0,100), r.Next(0,100));
+            //}
+            //foreach(KeyValuePair<int,int> item in boki)
+            //{
+            //    pola.Add(item.Key * item.Value);
+            //}
+            //foreach(KeyValuePair<int,int> j in boki)
+            //{
+            //    Console.WriteLine(j.Key + " " + j.Value);
+            //}
+            //foreach(int i in pola)
+            //{
+            //    Console.WriteLine(i + " ");
+            //}
+        }
+    }
+}
