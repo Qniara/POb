2f6faddddb5bbd73888a5c1b58b6cd7b2ac7bad8
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-30 12:17:07 +0100

    Add files via upload

---
+++ Lekcje/Classes/cw_30_01_2024/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_30_01_2024
+{
+    enum Color
+    {
+        Red,
+        Green,
+        Blue,
+        White,
+        Black,
+        Pink
+    }
+    internal class Car
+    {
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private Color Color { get; set; }
+        private int Year { get; set; }
+        public void ShowInformation()
+        {
+            Console.WriteLine($"This is {Brand} {Model} from {Year}, color {Color}");
+        }
+    }
+}
---
+++ Lekcje/Classes/cw_30_01_2024/Parking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_30_01_2024
+{
+    internal class Parking
+    {
+        private string Name { get; set; }
+        private string[] Cars { get; set; }
+        public void AddCar(string name)
+        {
+            
+        }
+        public void RemoveCar(int number)
+        {
+
+        }
+        public void ShowCars()
+        {
+            foreach( var car in Cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
