73c79723faaede9a5e7b84063674302c6fe50538
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-02-03 18:43:44 +0100

    Add files via upload

---
+++ zadania_cw/Horse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_do_lekcji_03_02_2024
+{
+    internal class Horse
+    {
+        enum sex
+        {
+            mare,
+            stallion,
+            gelding
+        }
+        public string name { get; set; }
+        public int age { get; set; }
+        private sex sex2 { get; set; }      
+        public string breed { get; set; }
+        public void SetName(string Name)
+        {
+            name = Name;
+        }
+    }
+}
---
+++ zadania_cw/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_do_lekcji_03_02_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Horse Basia = new Horse();
+            Basia.SetName("Basislaw");
+            Console.WriteLine(Basia.name);
+        }
+    }
+}
