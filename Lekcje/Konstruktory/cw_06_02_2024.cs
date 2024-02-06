895f1cbbefc2d4bab94b2ec7149880762fb893e5
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-02-06 11:39:07 +0100

    Create cw_06_02_2024.cs

---
+++ Lekcje/Konstruktory/cw_06_02_2024.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_06_02_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
