8a63359f58603ea30675a13a2b309c46481cbef0
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-02-06 12:16:26 +0100

    Update and rename Lekcje/Konstruktory/cw_06_02_2024.cs to Lekcje/Konstruktory/cw_06_02_2024/program.cs

--- Lekcje/Konstruktory/cw_06_02_2024/program.cs
+++ Lekcje/Konstruktory/cw_06_02_2024/program.cs
@@ -10,6 +10,8 @@ namespace zad_06_02_2024
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person("Kurt","Cobain");
+            Console.WriteLine(p1.Firstname + " " + p1.Lastname);
         }
     }
 }
