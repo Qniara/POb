ca37df5e167f44dd7de35b5f06697bd225545645
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-02-27 12:10:29 +0100

    Update Person.cs

--- Lekcje/Konstruktory/cw_27_02_2024/Person.cs
+++ Lekcje/Konstruktory/cw_27_02_2024/Person.cs
@@ -1,4 +1,6 @@
+using Microsoft.SqlServer.Server;
 using System;
+using System.Data.SqlTypes;
 
 namespace ConsoleApp1.Classes
 {
@@ -10,6 +12,8 @@ namespace ConsoleApp1.Classes
         //właściwości
         public string Name { get; set; }
         public string Surname { get; set; }
+        public int Age { get; set; }
+        public float Height { get; set; }
 
         //konstruktor statyczny jest wywoływany automatycznie aby zainicjować klase
         //przed utworzeniem 1 instanji. Konstruktor statyczny jest wywoływany
@@ -21,6 +25,68 @@ namespace ConsoleApp1.Classes
         {
             Console.WriteLine("Statyczny konstruktor klasy Person");
             Counter++;
+        }
+        //Konstruktor domyśly jest bezparamaetrowyadnego. Jeli klasa nie ma żadnego
+        //konstruktora to konstruktor domyślny jest wywoływany przy tworzeniu obiektu.
+        //Inicjuje on wszystkie pola do ich wartości domyślnych. Jeśli zdefiniujemy jakiś
+        //konstruktor prametryczny to nie otrzymamy automatycznie konstruktora domyślnego.
+
+        //Dodać konstruktor domyślny
+        public Person()
+        {
+            Console.WriteLine("Konstruktor domysny klasy Person");
+            Name = "BRUDNY";
+            Surname = "KIBEL";
+            Counter++;
+        }
+        //Konstrutor parametryczny ma co najmniej jeden parametr. Służy do inicjowania pól
+        //obiektu zgodnie z wartościami podanymi przy tworzeniu obiektu. Możemy mieć wiele
+        //konstruktorów parametrycznych, o ile różnią się liczbą lub typemparametrów.
+
+        //Konstruktor parametryczny z jednym parametrem
+        public Person(string name)
+        {
+            Console.WriteLine("Konstruktor parametryczny z jednym parametrem\r\n        public Person(string name)");
+            Name = name;
+            Counter++;
+        }
+
+        //Konstruktor parametryczny z dwoma parametrami
+        public Person(string name, string surname)
+        {
+            Console.WriteLine("Konstruktor parametryczny z dwoma parametrami");
+            Name = name;
+            Surname = surname;
+            Counter++;
+        }
+
+        //Konstruktor parametryczny z trzema parametrami
+        public Person(string name, string surname, int age)
+        {
+            Console.WriteLine("Konstruktor parametryczny z dwoma parametrami");
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Counter++;
+        }
+
+        //Konstruktor parametryzcny z czterema paramterami
+        //this służy do wywołania innego konstruktora tej samej klasy, czyli konstruktora 
+        //parametrycznego z trzema parametrami. Dzięki temu konstruktor z czterema
+        //parametrami ni emusi inicjować pól Name, Surname, Age, a może skupić się nadodaniu
+        //pola Height. Jest to sposób na uniknięcie powtarzania kodu i zapewnienie spojnści
+        //danych.
+        public Person(string name, string surname, int age, float height)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Height = height;
+            Counter++;
+        }
+        public string GetData()
+        {
+            return $"Imie i nazwisko:{Name} {Surname}";
         }
     }
 }
