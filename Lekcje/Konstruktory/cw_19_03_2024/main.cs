502d410d6718d1b802bed4df7143607930e51906
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-03-19 12:05:30 +0100

    Update main.cs

--- Lekcje/Konstruktory/cw_19_03_2024/main.cs
+++ Lekcje/Konstruktory/cw_19_03_2024/main.cs
@@ -1,1 +1,19 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+namespace cw_19_03_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime x = new DateTime(1994, 04, 05);
+            Samochod perpetummobile = new Samochod("BEEMWU", "NIEWIEM", 1967, 50, false, x, StatusSamochodu.zabytkowy);
+            perpetummobile.ShowInformation();
+            Console.WriteLine(perpetummobile.ObliczWiekSamochodu());
+        }
+    }
+}
