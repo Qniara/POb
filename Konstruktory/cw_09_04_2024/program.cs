e276e9a699938de0e4e63ab266819913599272b4
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-09 10:41:13 +0200

    Update program.cs

--- Konstruktory/cw_09_04_2024/program.cs
+++ Konstruktory/cw_09_04_2024/program.cs
@@ -10,7 +10,15 @@ namespace cw_09_04_2024
     {
         static void Main(string[] args)
         {
+            Car car1 = new Car("Ferrari", 300);
+            Car car2 = new Car("Porsche", 280);
+            Car car3 = new Car("Lamborghini", 320);
 
+            car1.StartRace();
+            car2.StartRace();
+            car3.StartRace();
+            car2 = null;
+            GC.Collect();
         }
     }
 }
