785431bc0324c69c0c3562667b2908261a89e128
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-09 10:44:44 +0200

    Create car.cs

---
+++ Lekcje/Konstruktory/cw_09_04_2024/car.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_09_04_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Ferrari", 300);
+            Car car2 = new Car("Porsche", 280);
+            Car car3 = new Car("Lamborghini", 320);
+
+            car1.StartRace();
+            car2.StartRace();
+            car3.StartRace();
+            car2 = null;
+            GC.Collect();
+        }
+    }
+}
