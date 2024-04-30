b6977f6a7cf6b218ad6b12679e3e3fdf0e724491
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-30 12:12:34 +0200

    Update cw_23_04_2024.cs

--- Lekcje/cw_23_04_2024.cs
+++ Lekcje/cw_23_04_2024.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management.Instrumentation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using static cw_23_04_2024.Program.Car;
 using static cw_23_04_2024.Program.Vehicle;
@@ -23,7 +24,8 @@ namespace cw_23_04_2024
             public string Brand { get; set; }
             public string Model { get; set; }
             //public double Fuel { get; private set; }
-            public FuelType Fueltype { get; set; }
+            public FuelType fueltype { get; set; }
+            public double Fuel { get; set; }
             public ushort Speed { get; set; }
             public void StartEngine()
             {
@@ -36,7 +38,6 @@ namespace cw_23_04_2024
         }
         public class Car : Vehicle
         {
-            public double Fuel { get; private set; }
             public int NumberOfDoors { get; set; }
             public int CurrentGear { get; set; }
             public int MaxGear { get; set; }
@@ -53,6 +54,10 @@ namespace cw_23_04_2024
             public void Refuel(double amount)
             {
                 Fuel += amount;
+                Console.WriteLine();
+            }
+            public void UpdateSpeed(ushort speed)
+            {
                 Console.WriteLine();
             }
             public void ChangeGear(byte gear)
@@ -114,8 +119,10 @@ namespace cw_23_04_2024
                 if (kwh <= 0) Console.WriteLine("\nNie mozna naladowac baterii ujemna wartoscia.");
                 else
                 {
-                    BatteryCapacity = kwh;
                     Console.WriteLine($"Naladowano baterie o {kwh} kwh.");
+                    AnimationHelper.ShowProgress("Ladowanie baterii: ", "zakonczone", 500, 10);
+                    BatteryCapacity += kwh;
+                    Console.WriteLine("\nNaladowano baterie o: " + kwh);
                 }
             }
         }
@@ -123,7 +130,21 @@ namespace cw_23_04_2024
         {
             public static void ShowProgress(string prefix, string suffix, int delay, int totalsteps)
             {
-
+                for(int i = 0; i <= totalsteps; i++)
+                {
+                    Console.Write($"\r{prefix}[{new string('|',i)}{new string('.',totalsteps-i)}]{suffix} {i*(100/totalsteps)}%");
+                    Thread.Sleep(delay);
+                }
+            }
+        }
+        public class Truck : Vehicle
+        {
+            public double LoadCapacity { get; set; }
+            public void LoadCargo(double weight)
+            {
+                if (weight <= 0) Console.WriteLine("\nNie mozan zaladowac ujemnej wagi");
+                else if (weight > LoadCapacity) Console.WriteLine("\nPrzekroczono limit wagi.");
+                else Console.WriteLine("\nZaladowano ladunek o wadze: " + weight);
             }
         }
         static void Main(string[] args)
@@ -133,18 +154,31 @@ namespace cw_23_04_2024
                 Brand = "Tesla",
                 Model = "S",
                 NumberOfDoors = 5,
-                Fueltype = FuelType.Electric,
+                fueltype = FuelType.Electric,
                 MaxGear = 1,
                 IsAutomatic = true,
                 BatteryCapacity = 95
             };
             tesla.StartEngine();
             tesla.StopEngine();
-            tesla.ChargeBattery(5);
+            tesla.ChargeBattery(10);
+            Console.WriteLine(tesla.BatteryCapacity);
+            Console.WriteLine();
+            Truck volvo = new Truck
+            {
+                Brand = "Volvo",
+                Model = "FH",
+                Fuel = 300,
+                fueltype = FuelType.Diesel,
+                LoadCapacity=25000
+            };
+            volvo.LoadCargo(5000);
+            volvo.StartEngine();
+            
 
-            Vehicle v = new Vehicle();
-            v.Brand = "Toster";
-            v.StartEngine();
+            //Vehicle v = new Vehicle();
+            //v.Brand = "Toster";
+            //v.StartEngine();
 
             //Car car = new Car();
             //car.Brand = "Fiat";
