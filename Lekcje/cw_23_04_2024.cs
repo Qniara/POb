82e7101c560ec0b68aafe3510b4e781cdc8bfdf4
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-30 10:58:39 +0200

    Update cw_23_04_2024.cs

--- Lekcje/cw_23_04_2024.cs
+++ Lekcje/cw_23_04_2024.cs
@@ -1,100 +1,171 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
+using static cw_23_04_2024.Program.Car;
+using static cw_23_04_2024.Program.Vehicle;
 
 namespace cw_23_04_2024
 {
     internal class Program
     {
-        public enum TypPaliwa
-        {
-            petrol, diesel, electric
-        }
         public class Vehicle
         {
+            public enum FuelType
+            {
+                Petrol,
+                Diesel,
+                Electric
+            }
+
             public string Brand { get; set; }
             public string Model { get; set; }
-            public double Fuel { get; private set; }
-            public TypPaliwa FuelType { get; protected set; }
+            //public double Fuel { get; private set; }
+            public FuelType Fueltype { get; set; }
             public ushort Speed { get; set; }
-            public virtual void StartEngine()
+            public void StartEngine()
             {
                 Console.WriteLine("Engine started");
             }
-            public virtual void StopEngine()
+            public void StopEngine()
             {
                 Console.WriteLine("Engine stopped");
+            }
+        }
+        public class Car : Vehicle
+        {
+            public double Fuel { get; private set; }
+            public int NumberOfDoors { get; set; }
+            public int CurrentGear { get; set; }
+            public int MaxGear { get; set; }
+            public bool IsAutomatic { get; set; }
+            public virtual void StartEngine()
+            {
+                base.StartEngine(); //przeciazenie
+                Console.WriteLine("Silnik samochodu uruchomiony"); //przyslanianie //"chleb pszenny","3.99f", new DataTime = 30.04.24r , true | zad zam - A, C, B, B (chyba xd)
+            }
+            public virtual void StopEngine()
+            {
+                Console.WriteLine("Silnik zatrzymany");
             }
             public void Refuel(double amount)
             {
                 Fuel += amount;
                 Console.WriteLine();
             }
-        }
-        public class Car : Vehicle
-        {
-            public int NumberOfDoors { get; set; }
-            public int CurrentGear { get; set; }
-            public int MaxGear { get; set; }
-            public bool IsAutomatic { get; set; }
-            public override void StartEngine()
-            {
-                //base.StartEngine(); //przeciazenie
-                Console.WriteLine("Silnik samochodu uruchomiony"); //przyslanianie
-            }
-            public override void StopEngine()
-            {
-                Console.WriteLine("Silnik zatrzymany");
-            }
             public void ChangeGear(byte gear)
             {
-                if(!IsAutomatic)
+                if (!IsAutomatic)
                 {
-                    if(gear<0 || gear > MaxGear)
+                    if (gear < 0 || gear > MaxGear)
                     {
-                        Console.WriteLine("Nieprawidlowy bieg");
+                        Console.WriteLine("\nNieprawidłowy bieg");
                     }
                     else
                     {
                         CurrentGear = gear;
-                        Console.WriteLine($"Zmiana biegu na {gear}");
+                        Console.WriteLine("\nZmiana biegu na {gear}");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("\nTen samochod ma automata");
+                    Console.WriteLine("\nSamochód ma automat");
                 }
             }
-            public void AutomaticGearChange(int speed)
+            private void AutomaticGearChange(int speed)
             {
-                if(IsAutomatic)
+                if (IsAutomatic)
                 {
-                    if (speed < 20) CurrentGear = 1;
-                    if (speed > 20 && speed < 40) CurrentGear = 3;
-                    else CurrentGear = 3;
-                    Console.WriteLine($"Automatyczna zmiana biegow na {CurrentGear} dla predkosci {speed}.");
+                    if (speed < 20)
+                    {
+                        CurrentGear = 1;
+                    }
+                    else if (speed > 20 && speed < 40)
+                    {
+                        CurrentGear = 2;
+                    }
+                    else
+                    {
+                        CurrentGear = 3;
+                    }
+                    Console.WriteLine("Automatyczna zmiana biegów na {CurrentGear} dla prędkości {speed} km/h");
                 }
                 else
                 {
-                    Console.WriteLine("Manualna skrzynia biegow");
+                    Console.WriteLine("\nManualna skrzynia biegów");
                 }
+            }
+        }
+        public class ElectricCar : Car
+        {
+            public double BatteryCapacity { get; set; }
+            public override void StartEngine()
+            {
+                base.StartEngine();
+            }
+            public override void StopEngine()
+            {
+                base.StopEngine();
+            }
+            public void ChargeBattery(double kwh)
+            {
+                if (kwh <= 0) Console.WriteLine("\nNie mozna naladowac baterii ujemna wartoscia.");
+                else
+                {
+                    BatteryCapacity = kwh;
+                    Console.WriteLine($"Naladowano baterie o {kwh} kwh.");
+                }
+            }
+        }
+        public static class AnimationHelper
+        {
+            public static void ShowProgress(string prefix, string suffix, int delay, int totalsteps)
+            {
+
             }
         }
         static void Main(string[] args)
         {
+            ElectricCar tesla = new ElectricCar
+            {
+                Brand = "Tesla",
+                Model = "S",
+                NumberOfDoors = 5,
+                Fueltype = FuelType.Electric,
+                MaxGear = 1,
+                IsAutomatic = true,
+                BatteryCapacity = 95
+            };
+            tesla.StartEngine();
+            tesla.StopEngine();
+            tesla.ChargeBattery(5);
+
             Vehicle v = new Vehicle();
             v.Brand = "Toster";
             v.StartEngine();
 
-            Car car = new Car();
-            car.Brand = "Fiat";
-            Console.WriteLine(car.Brand);
-            car.NumberOfDoors = 4;
+            //Car car = new Car();
+            //car.Brand = "Fiat";
+            //Console.WriteLine(car.Brand);
+            //car.NumberOfDoors = 4;
 
-            Car car1 = new Car { Brand="Toyota", Model="Avensis", NumberOfDoors= 4, FuelType = TypPaliwa.petrol};
+            //Car car1 = new Car { Brand = "Toyota", Model = "Avensis", NumberOfDoors = 4, FuelType = TypPaliwa.petrol };
+
+            //Vehicle v = new Vehicle();
+            //v.Brand = "Toster";
+            //v.StartEngine();
+
+            //Car car = new Car();
+            //car.Brand = "Fiat";
+            //Console.WriteLine(car.Brand);
+            //car.NumberOfDoors = 4;
+
+            //Car car1 = new Car { Brand = "Toyota", Model = "Avensis", NumberOfDoors = 4, FuelType = TypPaliwa.petrol };
+
+            //this song is dedicated to all the guns n fucking roses fans that stack with
+            //us through all the fucking shit
         }
     }
 }
