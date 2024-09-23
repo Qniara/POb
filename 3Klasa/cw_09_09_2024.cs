c08eba73c32840acc43d34b6e5f896f86f93b685
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-09-23 14:58:56 +0200

    Update cw_09_09_2024.cs

--- 3Klasa/cw_09_09_2024.cs
+++ 3Klasa/cw_09_09_2024.cs
@@ -1,126 +1,171 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static _1_dziedziczenie.Program;
 
-namespace ConsoleApp1
+namespace _1_dziedziczenie
 {
-	internal class Program
-	{
-		public class Shape
-		{
-			public virtual float CalculateArea()
-			{
-				return 0;
-			}
-			public virtual float CalculatePerimeter()
-			{
-				return 0;
-			}
-		}
-        public class  Rectangle : Shape
+    internal class Program
+    {
+        public class Shape
         {
-			private float width;
-			private float height;
+            public virtual float CalculateArea()
+            {
+                return 0;
+            }
 
-			public void SetDimension(float w, float h)
-			{
-				width = w;
-				height = h;
-			}
-
-			public override float CalculateArea()
-			{
-				return width * height;
-			}
-			public override float CalculatePerimeter()
-			{
-				return 2 * (height + width);
-			}
+            public virtual float CalculatePerimeter()
+            {
+                return 0;
+            }
         }
-		public class Circle : Shape
-		{
-			private float radius;
-			public Circle(float r)
-			{
-				radius = r;
-			}
-			public void SetRadius(float r)
-			{
-				radius = r;
-			}
-			public override float CalculateArea()
-			{
-				return (float)(2 * Math.PI * radius);
-			}
-		}
+
+        public class Rectangle : Shape
+        {
+            private float width;
+            private float height;
+
+            public void SetDimension(float w, float h)
+            {
+                width = w;
+                height = h;
+            }
+
+            public override float CalculateArea()
+            {
+                //return base.CalculateArea();//0
+                return width * height;
+            }
+
+            public override float CalculatePerimeter()
+            {
+                return 2 * (width + height);
+            }
+        }
+
+        public class Circle : Shape
+        {
+            private float radius;
+
+            public Circle(float r)
+            {
+                radius = r;
+            }
+
+            public void SetRadius(float r)
+            {
+                radius = r;
+            }
+
+            public override float CalculateArea()
+            {
+                return (float)Math.Round((Math.PI * radius * radius), 2);
+            }
+
+            public override float CalculatePerimeter()
+            {
+                return (float)Math.Round((2 * Math.PI * radius), 2);
+            }
+        }
+        public class Triangle : Shape
+        {
+            public float sideA;
+            public float sideB;
+            public float sideC;
+            public Triangle(float sideA, float sideB, float sideC)
+            {
+                this.sideA=sideA;
+                this.sideB=sideB;
+                this.sideC=sideC;
+            }
+            public override float CalculateArea()
+            {
+                float s = (sideA + sideB + sideC) / 2;
+                return s;
+            }
+        }
         static void Main(string[] args)
-		{
-			while(true)
-			{
-				Console.WriteLine("1. Prostokąt");
-				Console.WriteLine("2. Koło");
-				Console.WriteLine("3. Trójkąt");
-				Console.WriteLine("4. Trapez");
-				Console.WriteLine("5. Kula");
-				Console.WriteLine("6. Wyjście");
-				Console.Write("Wybiarz kształt do obliczenia: ");
-				int choice = int.Parse(Console.ReadLine());
-				switch (choice)
-				{
-					case 1:
-						Rectangle rect = new Rectangle();
-						Console.Write("Podaj szerokosc: ");
-						float RectWidth = float.Parse(Console.ReadLine());
-						Console.WriteLine();
-						Console.Write("Podaj wysokość: ");
-						float rectHight = float.Parse(Console.ReadLine());
-						rect.SetDimension(RectWidth, rectHight);
-						Console.WriteLine("Powierzchnia prostokąta {0}", rect.CalculateArea());
-						Console.WriteLine("Obwód prostokąta {0}", rect.CalculatePerimeter());
-						Console.ReadKey();
-						Console.Clear();
-						break;
-					case 2:
-						float cricRadius = GetValidInput("Podaj promien: ");
-						Circle circ = new Circle(circRadius);
-						Console.WriteLine("Powierzchnia kola {0}", circ.CalculateArea());
-						Console.WriteLine("Obwód kola {0}", circ.CalculatePerimeter());
-						Console.ReadKey();
-						Console.Clear();
-						break;
-					case 3:
-						break;
-					case 4:
-						break;
-					case 5:
-						break;
-					case 6:
-						return;
-					default:
-						Console.WriteLine("Nieprawidłowy wybór spróboj ponownie.");
-						break;
-				}
-			}
-		}
-		private static float GetValidInput(string prompt)
-		{
-			float result;
-			while (true)
-			{
-				Console.Write(prompt);
-				if (float.TryParse(Console.ReadLine(), out result) && result>0)
-				{
-					return result;
-				}
-				else
-				{
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine("Nieprawidlowe dane");
-					Console.ResetColor();
-				}
-			}
-		}
-	}
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Prostokąt");
+                Console.WriteLine("2. Koło");
+                Console.WriteLine("3. Trójkąt");
+                Console.WriteLine("4. Trapez");
+                Console.WriteLine("5. Kula");
+                Console.WriteLine("6. Wyjście");
+                Console.Write("Wybierz kształt do obliczenia:");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Rectangle rect = new Rectangle();
+                        Console.Write("Podaj szerokość:");
+                        float rectWith = float.Parse(Console.ReadLine());
+                        Console.Write("Podaj wysokość:");
+                        float rectHeight = float.Parse(Console.ReadLine());
+                        rect.SetDimension(rectWith, rectHeight);
+                        Console.WriteLine("Powierzchnia prostokąta: {0}", rect.CalculateArea());
+                        Console.WriteLine("Obwód prostokąta: {0}", rect.CalculatePerimeter());
+                        break;
+                    case 2:
+                        float circRadius = GetValidInput("Podaj promień:");
+                        Circle circ = new Circle(circRadius);
+                        Console.WriteLine("Powierzchnia koła: {0}", circ.CalculateArea());
+                        Console.WriteLine("Obwód koła: {0}", circ.CalculatePerimeter());
+                        break;
+                    case 3:
+                        float sideA, sideB, sideC;
+                        do
+                        {
+                            sideA = GetValidInput("Podaj dlugosc boku A: ");
+                            sideB = GetValidInput("Podaj dlugosc boku B: ");
+                            sideC = GetValidInput("Podaj dlugosc boku C: ");
+                            if(IsValidTriangle(sideA, sideB, sideC))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nNieprawidlowe dane. Sproboj ponownie.\n");
+                                Console.ResetColor();
+                            }
+                        }
+                        while (!IsValidTriangle(sideA, sideB, sideC));
+                        break;
+                    default:
+                        Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
+                        break;
+
+                }
+            }
+        }
+
+        private static bool IsValidTriangle(float sideA, float sideB, float sideC)
+        {
+            return (sideA + sideB > sideC) && (sideC + sideA > sideB) && (sideB + sideC > sideA);
+        }
+
+        private static float GetValidInput(string prompt)
+        {
+            float result;
+            while (true)
+            {
+                Console.Write(prompt);
+                if (float.TryParse(Console.ReadLine(), out result) && result > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Nieprawidłowe dane. Spróbuj ponownie.");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
 }
