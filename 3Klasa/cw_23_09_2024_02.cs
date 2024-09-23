3b02d58b9410a2891ba896d0cbd04c93ec6fa06d
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-09-23 15:08:30 +0200

    Create cw_23_09_2024_02.cs

---
+++ 3Klasa/cw_23_09_2024_02.cs
@@ -0,0 +1,16 @@
+//dziedziczenie, zarządzanie, biblioteka
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
